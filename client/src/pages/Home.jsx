3995d0ca9f6f5bec7e9fd856483678022fbcfc44
Gaurav Singh <76627244+Thatsgaurav@users.noreply.github.com>
2024-03-22 18:25:52 +0530

    Complete the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
+import { useEffect, useState } from "react";
+import PostCard from "../components/PostCard";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await fetch("/api/post/getPosts");
+      const data = await res.json();
+      setPosts(data.posts);
+    };
+    fetchPosts();
+  }, []);
   return (
-    <div>Home</div>
-  )
+    <div>
+      <div className="flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto ">
+        <h1 className="text-3xl font-bold lg:text-6xl">Welcome to my Blog</h1>
+        <p className="text-gray-500 text-xs sm:text-sm">
+          Here you'll find a diverse collection of articles and tutorials
+          covering a wide range of topics, including web development, software
+          engineering, and programming languages. Whether you're a seasoned
+          developer looking to enhance your skills or a beginner eager to dive
+          into the world of coding, there's something here for everyone. Explore
+          insightful articles that delve into the latest trends and technologies
+          shaping the digital landscape. From front-end frameworks to backend
+          systems, from mobile app development to machine learning algorithms, I
+          strive to provide valuable content that both educates and inspires.
+          Join me on this journey of exploration and discovery as we navigate
+          through the ever-evolving realm of technology. Whether you're seeking
+          practical coding tips, in-depth tutorials, or simply looking to stay
+          updated on industry news, you've come to the right place. So grab a
+          cup of coffee, sit back, and let's embark on this exciting adventure
+          together. Welcome to a world where curiosity meets code, and
+          innovation knows no bounds.
+        </p>
+        <Link
+          to="/search"
+          className="text-xs sm:text-sm text-teal-500 font-bold hover:underline"
+        >
+          View all posts
+        </Link>
+      </div>
+      <div className="p-3 bg-amber-100 dark:bg-slate-700">
+        <CallToAction />
+      </div>
+
+      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+        {posts && posts.length > 0 && (
+          <div className="flex flex-col gap-6">
+            <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
+            <div className="flex flex-wrap gap-4">
+              {posts.map((post) => (
+                <PostCard key={post._id} post={post} />
+              ))}
+            </div>
+            <Link
+              to={"/search"}
+              className="text-lg text-teal-500 hover:underline text-center"
+            >
+              View all posts
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
